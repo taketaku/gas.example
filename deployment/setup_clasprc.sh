931b01c87eb10d73e9c2eb44cead831b1427b580
taketaku <vbvbsp48@gmail.com>
2022-06-23 15:10:47 +0900

    rerefix

--- deployment/setup_clasprc.sh
+++ deployment/setup_clasprc.sh
@@ -5,7 +5,7 @@ LOGIN=$(cat <<-END
         "token": {
             "access_token": "$CLASPRC_ACCESS_TOKEN",
             "refresh_token": "$CLASPRC_REFRESH_TOKEN",
-            "scope": "https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/service.management https://www.googleapis.com/auth/script.deployments https://www.googleapis.com/auth/logging.read https://www.googleapis.com/auth/script.webapp.deploy https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/script.projects https://www.googleapis.com/auth/drive.metadata.readonly",
+            "scope": "https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/logging.read https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/script.projects openid https://www.googleapis.com/auth/script.deployments https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/service.management https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/script.webapp.deploy https://www.googleapis.com/auth/cloud-platform",
             "token_type": "Bearer",
             "id_token": "$CLASPRC_ID_TOKEN",
             "expiry_date": "$CLASPRC_EXPIRY_DATE"
