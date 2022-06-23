f45f134bde0f7ec92d3e3c69a04c9c1ba0014efd
taketaku <vbvbsp48@gmail.com>
2022-06-23 16:59:28 +0900

    refix

--- deployment/setup_clasprc.sh
+++ deployment/setup_clasprc.sh
@@ -7,7 +7,7 @@ LOGIN=$(cat <<-END
             "scope": "https://www.googleapis.com/auth/script.webapp.deploy https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/script.projects https://www.googleapis.com/auth/drive.metadata.readonly openid https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/script.deployments https://www.googleapis.com/auth/logging.read https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/service.management",
             "token_type": "Bearer",
             "id_token": "$CLASPRC_ID_TOKEN",
-            "expiry_date": $CLASPRC_EXPIRY_DATE,
+            "expiry_date": "$CLASPRC_EXPIRY_DATE",
             "refresh_token": "$CLASPRC_REFRESH_TOKEN"
         },
         "oauth2ClientSettings": {
@@ -21,3 +21,4 @@ END
 )
 
 echo $LOGIN > ~/.clasprc.json
+# echo $LOGIN > ./test.json
